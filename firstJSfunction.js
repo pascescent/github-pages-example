2b8f4a926d605b7bc59d0ef78c1cc8e4f89976f4
Дима <pascescent@gmail.com>
2022-11-16 00:27:37 +0700

    Римские цыферки

--- firstJSfunction.js
+++ firstJSfunction.js
@@ -1,25 +1,22 @@
-function orderWeight(string) {
-  let arr = string.split(' ').filter(x => x !== '').sort();
-  let per = {}
-  function numWeight (num) {
-    let numArr = num.split('');
-    for (let j = 0; j < numArr.length; j++) {
-      numArr[j] = Number(numArr[j]);
-    };
+
+function solution(number){
+  let romNumber = '';
+  let nums = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  let letters = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+  let count = nums.length - 1;
+
+  while (number > 0) {
+    if (number >= nums[count]) {
+      romNumber += letters[count];
+      number -= nums[count];
+    } else {
+      count--;
+    }
     
-    return numArr.reduce((sum, current) => {
-      return sum + current;
-    }, 0)
   }
-  let sortedArr = arr.sort((a, b) => {
-    return numWeight(a) - numWeight(b);
-  });
-  return sortedArr.join(' ')
+
+
+  return romNumber;
 }
 
-
-
-console.log(orderWeight('2000 10003 1234000 44444444 9999 11 11 22 123'))
-
-console.log('Привет мир!')
-
+console.log(solution(1001))
