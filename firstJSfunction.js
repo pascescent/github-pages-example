5704a38263588613303b316e00611114a1029c28
Дима <pascescent@gmail.com>
2022-11-17 00:43:06 +0700

    Считаем торты Пита

--- firstJSfunction.js
+++ firstJSfunction.js
@@ -1,9 +1,28 @@
-function arrayDiff(a, b) {
-  let newArr = [];
-    newArr = b.reduce((arr, exept) => {
-      return arr.filter(e => e !== exept)
-    }, a);
-  return newArr;
+let recipe = {flour: 500, sugar: 200, eggs: 1};
+let available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
+
+let portions = [];
+
+for (let key in recipe) {
+  if (available[key] / recipe[key] > 0) {
+    portions.push(Math.floor(available[key] / recipe[key]));
+  } else {
+    portions.push(0)
+  }
+  
 }
 
-console.log(arrayDiff([1, 2, 3, 4, 5, 2, 4], [2, 3]))
+console.log(portions)
+
+let count = 1;
+let min = portions[0];
+
+while (count < portions.length) {
+  if(portions[count] < min) {
+    min = portions[count]
+    count = 1;
+  }
+  count++;
+}
+
+console.log(min)
