e90b31957217bf68d0e6f36df4be3aa9d1958258
Дима <pascescent@gmail.com>
2022-11-19 21:37:18 +0700

    Скобачке

--- firstJSfunction.js
+++ firstJSfunction.js
@@ -1,27 +1,20 @@
-let url = 'https://www.google.ccadsfom';
-let newUrl = '';
+let parens = '(())(())(()(()()()(())))'
 
+let arr = parens.split('')
 
+let i = 0
 
-
-if (url.startsWith('http://')) {
-    url = url.slice(7)
-} else if (url.startsWith('https://')) {
-    url = url.slice(8)
+while (arr.length > 1) {
+    if (arr[0] === ')' || !arr.includes(')')) {
+        break
+    } 
+    arr.splice(0, 1);
+    arr.splice(arr.indexOf(')'), 1);
 }
 
-if (url.startsWith('www.')) {
-    url = url.slice(4)
-}
-
-newUrl = url.slice(0, url.indexOf('.') - url.length)
 
 
-console.log(newUrl)
-
-
-
-
+console.log(!arr.join(''))
 
 
 
